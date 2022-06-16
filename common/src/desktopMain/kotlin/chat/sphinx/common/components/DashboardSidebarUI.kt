ddd13b75516ce07d75a6f0b3eacd2ddf90b2c28e
kngako <kpangako+github@gmail.com>
2022-06-16 02:01:32 +0200

    Loading wheel+lightning color on header
    https://github.com/stakwork/sphinx-kotlin-ui/issues/39

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/DashboardSidebarUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/DashboardSidebarUI.kt
@@ -8,29 +8,29 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FlashOn
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import chat.sphinx.common.viewmodel.DashboardViewModel
 import chat.sphinx.di.container.SphinxContainer
+import chat.sphinx.response.LoadResponse
+import chat.sphinx.response.Response
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @Composable
-fun DashboardSidebarUI(dashboardStore: DashboardViewModel) {
+fun DashboardSidebarUI(dashboardViewModel: DashboardViewModel) {
     val scope = SphinxContainer.appModule.applicationScope
     val dispatchers = SphinxContainer.appModule.dispatchers
     var text by remember { mutableStateOf(TextFieldValue("")) }
     val balance = mutableStateOf(0L)
 
     scope.launch(dispatchers.main) {
-        dashboardStore.getAccountBalance().collect {
+        dashboardViewModel.getAccountBalance().collect {
             it?.let { nodeBalance ->
                 balance.value = nodeBalance.balance.value
             }
@@ -46,7 +46,7 @@ fun DashboardSidebarUI(dashboardStore: DashboardViewModel) {
                 backgroundColor = androidx.compose.material3.MaterialTheme.colorScheme.background,
                 elevation = 8.dp,
                 navigationIcon = {
-                    IconButton(onClick = dashboardStore::networkRefresh) {
+                    IconButton(onClick = dashboardViewModel::networkRefresh) {
                         Icon(
                             Icons.Default.Refresh,
                             contentDescription = "Refresh Connection",
@@ -55,8 +55,28 @@ fun DashboardSidebarUI(dashboardStore: DashboardViewModel) {
                     }
                 },
                 actions = {
-                    IconButton(onClick = {}) {
-                        Icon(Icons.Default.FlashOn, contentDescription = "Connection Status", tint = androidx.compose.material3.MaterialTheme.colorScheme.tertiary, modifier = Modifier.size(14.dp))
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        val networkState by dashboardViewModel.networkStateFlow.collectAsState()
+                        if (networkState is LoadResponse.Loading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(14.dp),
+                                strokeWidth = 2.dp
+                            )
+                        } else {
+                            Icon(
+                                Icons.Default.FlashOn,
+                                contentDescription = "Connection Status",
+                                tint = if (networkState is Response.Success) {
+                                    Color.Green
+                                } else {
+                                    androidx.compose.material3.MaterialTheme.colorScheme.error
+                                } ,
+                                modifier = Modifier.size(14.dp)
+                            )
+
+                        }
                     }
                 }
             )
