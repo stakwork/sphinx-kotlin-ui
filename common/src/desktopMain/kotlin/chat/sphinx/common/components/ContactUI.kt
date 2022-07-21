eda83b4ecd49c67131967f759e5d7cea6f89f831
Tom <tomasalbertotiminskas@gmail.com>
2022-07-21 16:09:25 -0300

    QRDetail moved to separated file

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/ContactUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/ContactUI.kt
@@ -2,28 +2,19 @@ package chat.sphinx.common.components
 
 import CommonButton
 import Roboto
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.QrCodeScanner
-import androidx.compose.material.icons.filled.TouchApp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -433,111 +424,6 @@ fun ContactForm(dashboardViewModel: DashboardViewModel, contactId: ContactId?) {
     }
     if (dashboardViewModel.qrWindowStateFlow.collectAsState().value){
         QRDetail(dashboardViewModel, QRCodeViewModel(viewModel.contactState.lightningNodePubKey, null))
-    }
-}
-
-@Composable
-fun QRDetail(
-    dashboardViewModel: DashboardViewModel,
-    viewModel: QRCodeViewModel
-){
-    val density = LocalDensity.current
-    val clipboardManager: ClipboardManager = LocalClipboardManager.current
-
-    var isOpen by remember { mutableStateOf(true) }
-    if (isOpen) {
-        Window(
-            onCloseRequest = {
-                dashboardViewModel.toggleQRWindow(false)
-            },
-            title = "",
-            state = WindowState(
-                position = WindowPosition.Aligned(Alignment.Center),
-                size = getPreferredWindowSize(357, 550)
-            ),
-            resizable = false
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize()
-                    .background(color = androidx.compose.material3.MaterialTheme.colorScheme.background)
-                    .clickable {
-                        clipboardManager.setText(AnnotatedString(viewModel.contactQRCodeState.pubKey))
-                    }
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(top = 38.dp),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "PUBLIC KEY",
-                        fontFamily = SphinxFonts.montserratFamily,
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
-
-                    Spacer(Modifier.height(24.dp))
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding(end = 24.dp)
-                    ) {
-                        Icon(
-                            Icons.Default.TouchApp,
-                            contentDescription = "QR Code",
-                            tint = Color.Gray,
-                            modifier = Modifier.size(30.dp)
-                        )
-
-                        Text(
-                            text = "CLICK TO COPY",
-                            fontFamily = SphinxFonts.montserratFamily,
-                            color = Color.Gray,
-                            fontSize = 10.sp
-                        )
-                    }
-
-                    Spacer(Modifier.height(18.dp))
-
-                    viewModel.contactQRCodeState.bitMatrix?.let { bitMatrix ->
-                        val width = bitMatrix.width
-                        val height = bitMatrix.height
-
-                        val widthDp = with(density) { width.toDp() }
-                        val heightDp = with(density) { height.toDp() }
-
-                        Box(modifier = Modifier.height(heightDp).width(widthDp)) {
-                            Canvas(modifier = Modifier.height(heightDp).width(widthDp)) {
-                                for (x in 0 until width) {
-                                    for (y in 0 until height) {
-                                        drawRect(
-                                            brush = SolidColor(if (bitMatrix.get(x, y)) Color.Black else Color.White),
-                                            topLeft = Offset(x.toFloat(), y.toFloat()),
-                                            size = Size(1f, 1f)
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    Spacer(Modifier.height(24.dp))
-
-                    Text(
-                        modifier = Modifier.fillMaxWidth().padding(20.dp),
-                        text = viewModel.contactQRCodeState.pubKey,
-                        maxLines = 2,
-                        fontFamily = SphinxFonts.montserratFamily,
-                        color = Color.Gray,
-                        fontSize = 11.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
-        }
     }
 }
 
