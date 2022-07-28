fa6acd995c5ed2124edea5c8feecfa5eb7f3b81e
kngako <kpangako+github@gmail.com>
2022-07-28 21:05:02 +0200

    Ideal notifications UX
    https://github.com/stakwork/sphinx-kotlin-ui/issues/31#issuecomment-1192818545

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/notifications/DesktopSphinxNotifications.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/notifications/DesktopSphinxNotifications.kt
@@ -12,9 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -22,9 +20,6 @@ import androidx.compose.ui.awt.ComposeWindow
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -32,21 +27,14 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.rememberWindowState
 import chat.sphinx.common.components.notifications.DesktopSphinxNotificationManager.notifications
-import chat.sphinx.utils.getFullscreenWindowSize
 import chat.sphinx.utils.getPreferredWindowSize
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import java.awt.Dimension
 import java.awt.MouseInfo
 import java.awt.event.MouseEvent
-import java.awt.event.MouseListener
-import java.awt.event.MouseMotionAdapter
 import java.awt.event.MouseMotionListener
-import javax.swing.event.MouseInputAdapter
 
 /**
  * This is a hack to have notifications on all platforms... Could possibly only use it for linux as the mac/windows notifications should work will.
@@ -70,33 +58,58 @@ fun DesktopSphinxNotifications(
             alwaysOnTop = true,
             transparent = true,
             resizable = true,
+            focusable = false,
             undecorated = true,
             icon = icon
         ) {
             val scope = rememberCoroutineScope()
             var removeNotificationsJob: Job? = null
-            val sphinxMouseMotionListener = object: MouseMotionListener {
+
+            val initialLocation = MouseInfo.getPointerInfo().location
+
+            val pointerLocation = mutableStateOf(
+                initialLocation
+            )
+
+            LaunchedEffect(initialLocation) {
+                do {
+                    delay(700)
+                    pointerLocation.value = MouseInfo.getPointerInfo().location
+                } while (initialLocation.equals(pointerLocation.value))
+
+                // Clear notifications...
+                if (removeNotificationsJob?.isActive == true) {
+                    return@LaunchedEffect
+                }
+                // Remove notifications when cursor moves...
+                removeNotificationsJob = scope.launch {
+                    delay(800)
+                    notifications.clear()
+                }
+            }
+
+            val sphinxReadingNotificationsListener = object: MouseMotionListener {
                 override fun mouseDragged(e: MouseEvent) {
                 }
 
                 override fun mouseMoved(e: MouseEvent) {
                     if (notifications.isNotEmpty()) {
                         if (e.id == MouseEvent.MOUSE_MOVED) {
-                            if (removeNotificationsJob?.isActive == true) {
-                                return
-                            }
-                            // Remove notifications when cursor moves...
-                            removeNotificationsJob = scope.launch {
-                                delay(500)
-                                notifications.clear()
+                            // If the mouse is moved anywhere on the notifications we are reading them
+                            removeNotificationsJob?.let {
+                                if (it.isActive) {
+                                    it.cancel()
+                                    return
+                                }
                             }
                         }
                     }
                 }
             }
 
-            dashboardWindow.addMouseMotionListener(
-                sphinxMouseMotionListener
+            // Notifications will be removed when the mouse moves... unless the mouse is over them
+            window.addMouseMotionListener(
+                sphinxReadingNotificationsListener
             )
 
             Box(modifier = Modifier.fillMaxSize())
