4479f53d682a48e6b2eb496867e463bf4e3727cf
Alejandro <ariel10aguero@gmail.com>
2022-09-21 18:23:53 -0300

    Save audio message as attachment

--- common/src/commonMain/kotlin/chat/sphinx/common/components/MessageAudio.kt
+++ common/src/commonMain/kotlin/chat/sphinx/common/components/MessageAudio.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import chat.sphinx.common.models.ChatMessage
 import chat.sphinx.common.viewmodel.chat.ChatViewModel
@@ -20,28 +19,44 @@ fun MessageAudio(
     chatMessage: ChatMessage,
     chatViewModel: ChatViewModel
 ) {
-    val localFilepath = chatMessage.message.messageMedia?.localFile
+    val message = chatMessage.message
+    val messageMedia = message.messageMedia
+    val localFilepath = messageMedia?.localFile
+    val url = messageMedia?.url?.value ?: ""
+
+    LaunchedEffect(url) {
+        chatViewModel.downloadFileMedia(message, chatMessage.isSent)
+    }
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.height(62.dp).padding(12.dp)
+        modifier = Modifier.height(68.dp).padding(start = 8.dp, top = 12.dp, bottom = 12.dp, end = 12.dp)
     ) {
-        if (localFilepath != null) {
-            Icon(
-                Icons.Default.PlayArrow,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.tertiary
-            )
+        Box(
+            modifier = Modifier.size(48.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            if (localFilepath != null) {
+                IconButton(
+                    onClick = { toast("Please download the file, audio player not implemented yet") }
+                ) {
+                    Icon(
+                        Icons.Default.PlayArrow,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.tertiary,
+                    )
+                }
+            } else {
+                CircularProgressIndicator(
+                    strokeWidth = 2.dp,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    modifier = Modifier.size(28.dp)
+                )
+            }
         }
-        else {
-            CircularProgressIndicator(
-                strokeWidth = 2.dp,
-                color = MaterialTheme.colorScheme.tertiary,
-                modifier = Modifier.size(20.dp)
-            )
-        }
-        Box(modifier = Modifier.fillMaxWidth(0.4f).padding(start = 12.dp, end = 12.dp)
+        Box(
+            modifier = Modifier.fillMaxWidth(0.4f).padding(start = 8.dp, end = 12.dp)
         ) {
             val slideValue = remember { mutableStateOf(0f) }
             Slider(
@@ -56,18 +71,20 @@ fun MessageAudio(
                 )
             )
         }
-//        if (chatMessage.message.messageMedia?.localFile != null ) {
-//            chatMessage.message.messageMedia?.localFile?.let { path ->
-//                val audioInputStream = AudioSystem.getAudioFileFormat(path.toFile())
+//        if (localFilepath != null) {
+//            try {
+//                val audioInputStream = AudioSystem.getAudioFileFormat(localFilepath.toFile())
 //                val format = audioInputStream.format
 //                val audioFileLength = audioInputStream.frameLength
 //                val frameSize = format.frameSize
 //                val frameRate = format.frameRate
 //                val durationInSeconds = (audioFileLength / (frameSize * frameRate))
-//                println("duration $durationInSeconds")
+//            }
+//            catch (e: Exception) {
+//                println("Duration Error: $e ")
 //            }
 //        }
 
-        Text("0.06", color = MaterialTheme.colorScheme.tertiary)
+        Text("01:00", color = MaterialTheme.colorScheme.tertiary)
     }
 }
