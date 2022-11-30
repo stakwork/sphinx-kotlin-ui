0e6be57cdcb1ce3e3e181557165c205a40a8a2af
Tom <tomasalbertotiminskas@gmail.com>
2022-11-30 12:46:18 -0300

    Compose upgrade from 1.0.1 to 1.1.1

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.compose
 plugins {
     id("com.android.library")
     kotlin("multiplatform")
-    id("org.jetbrains.compose") version "1.0.1"
+    id("org.jetbrains.compose") version "1.1.0"
 
 }
 
