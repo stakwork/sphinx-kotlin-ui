35fedceddaac61590b6e3cded7caed9f994bf9f2
Alejandro <ariel10aguero@gmail.com>
2022-12-02 01:07:16 -0300

    Create user row and set up lazy column

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/tribe/LeaderboardUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/tribe/LeaderboardUI.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -24,12 +28,16 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import chat.sphinx.common.Res
+import chat.sphinx.common.components.LoadingRow
 import chat.sphinx.common.components.PhotoUrlImage
+import chat.sphinx.common.components.PodcastBoost
+import chat.sphinx.common.components.TransactionRow
 import chat.sphinx.common.viewmodel.DashboardViewModel
 import chat.sphinx.platform.imageResource
 import chat.sphinx.utils.SphinxFonts
 import chat.sphinx.utils.getPreferredWindowSize
 import chat.sphinx.wrapper.PhotoUrl
+import theme.light_divider
 
 @Composable
 fun LeaderboardUI(dashboardViewModel: DashboardViewModel) {
@@ -46,49 +54,61 @@ fun LeaderboardUI(dashboardViewModel: DashboardViewModel) {
                 size = getPreferredWindowSize(420, 620)
             )
         ) {
-            LeaderboardScreen()
-        }
-    }
-}
+            val listState = rememberLazyListState()
 
-@Composable
-fun LeaderboardScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.onSurfaceVariant)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth().height(260.dp).background(MaterialTheme.colorScheme.onSurfaceVariant),
-            verticalArrangement = Arrangement.Bottom
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.Center
+            Column(
+                modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.onSurfaceVariant)
             ) {
-                SmallProfile("2")
                 Column(
-                    modifier = Modifier.fillMaxHeight().wrapContentWidth(),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier.fillMaxWidth().height(260.dp).background(MaterialTheme.colorScheme.onSurfaceVariant),
+                    verticalArrangement = Arrangement.Bottom
                 ) {
-                    Spacer(Modifier.height(24.dp))
-                    Box(
-                        modifier = Modifier.height(30.dp).width(45.dp),
-                        contentAlignment = Alignment.BottomCenter
-                    ){
-                        Image(
-                            painter = imageResource(Res.drawable.ic_crown),
-                            contentDescription = "crown icon",
-                            contentScale = ContentScale.Crop
-                        )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.Bottom,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        SmallProfile("2")
+                        Column(
+                            modifier = Modifier.fillMaxHeight().wrapContentWidth(),
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(Modifier.height(24.dp))
+                            Box(
+                                modifier = Modifier.height(30.dp).width(45.dp),
+                                contentAlignment = Alignment.BottomCenter
+                            ){
+                                Image(
+                                    painter = imageResource(Res.drawable.ic_crown),
+                                    contentDescription = "crown icon",
+                                    contentScale = ContentScale.Crop
+                                )
+                            }
+                            BigProfile("1")
+                        }
+                        SmallProfile("3")
                     }
-                    BigProfile("1")
                 }
-                SmallProfile("3")
+                Spacer(modifier = Modifier.height(28.dp))
+                Box(
+                    modifier = Modifier.fillMaxSize()
+
+                ) {
+                    LazyColumn(
+                        state = listState,
+                        contentPadding = PaddingValues(4.dp)
+                    ) {
+                        items(count = 16) {
+                            UsersRow()
+                        }
+                    }
+                }
             }
         }
     }
 }
+
 
 @Composable
 fun SmallProfile(position: String) {
@@ -134,8 +154,8 @@ fun SmallProfile(position: String) {
                     fontWeight = FontWeight.ExtraLight,
                     fontFamily = Roboto,
                     textAlign = TextAlign.Center,
-                    fontSize = 17.sp,
-                    modifier = Modifier.padding(bottom = 8.dp)
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    fontSize = 17.sp
                 )
                 Spacer(Modifier.width(6.dp))
                 Image(
@@ -206,6 +226,74 @@ fun BigProfile(position: String) {
         }
     }
 }
+
+@Composable
+fun UsersRow(){
+    Row (
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column (
+            modifier = Modifier.weight(2f).height(64.dp).padding(8.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(start = 8.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                PhotoUrlImage(PhotoUrl("empty"))
+                Spacer(Modifier.width(16.dp))
+                Text(
+                    text = "Thomas",
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontWeight = FontWeight.ExtraLight,
+                    fontFamily = Roboto,
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp
+                )
+            }
+        }
+        Column (
+            modifier = Modifier.weight(1f).height(64.dp).padding(end = 24.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "1662",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.ExtraLight,
+                    fontFamily = Roboto,
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp
+                )
+                Spacer(Modifier.width(42.dp))
+                Text(
+                    text = "1",
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontWeight = FontWeight.ExtraLight,
+                    fontFamily = Roboto,
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp
+                )
+                Spacer(Modifier.width(8.dp))
+                Image(
+                    // TODO add if statement to include ic_polygon_down and handle down position
+                    painter = imageResource(Res.drawable.ic_polygon_up),
+                    contentDescription = "position up and down",
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+            }
+        }
+    }
+    Divider(modifier = Modifier.fillMaxWidth().padding(start = 68.dp, end = 20.dp), color = light_divider)
+
+}
+
+
 
 @Preview
 @Composable
