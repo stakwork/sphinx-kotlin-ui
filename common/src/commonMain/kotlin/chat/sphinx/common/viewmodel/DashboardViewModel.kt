aed9a646e4a26de0539f530c2edb7e47404ff0ec
Tom <tomasalbertotiminskas@gmail.com>
2022-12-19 17:44:59 -0300

    Fix on package version getter

--- common/src/commonMain/kotlin/chat/sphinx/common/viewmodel/DashboardViewModel.kt
+++ common/src/commonMain/kotlin/chat/sphinx/common/viewmodel/DashboardViewModel.kt
@@ -172,7 +172,7 @@ class DashboardViewModel: WindowFocusListener {
     }
 
     private fun getPackageVersion(){
-        val currentAppVersion = System.getProperty("jpackage.app-version") ?: "1.0.15"
+        val currentAppVersion = "1.0.15"
 
         viewModelScope.launch(dispatchers.mainImmediate) {
             networkQueryVersion.getAppVersions().collect { loadResponse ->
