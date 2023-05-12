baa5de4729ba72e4577a2de7a03556b5be7e0302
Tom <tomasalbertotiminskas@gmail.com>
2023-05-12 10:30:41 -0300

    New version 1.0.18

--- common/src/commonMain/kotlin/chat/sphinx/common/viewmodel/DashboardViewModel.kt
+++ common/src/commonMain/kotlin/chat/sphinx/common/viewmodel/DashboardViewModel.kt
@@ -172,7 +172,7 @@ class DashboardViewModel: WindowFocusListener {
     }
 
     private fun getPackageVersion(){
-        val currentAppVersion = "1.0.17"
+        val currentAppVersion = "1.0.18"
 
         viewModelScope.launch(dispatchers.mainImmediate) {
             networkQueryVersion.getAppVersions().collect { loadResponse ->
