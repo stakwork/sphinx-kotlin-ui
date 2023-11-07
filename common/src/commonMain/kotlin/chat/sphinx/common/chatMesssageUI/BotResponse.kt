7812e1005f24be09feb9f16b4440bc11451eb67d
Alejandro <ariel10aguero@gmail.com>
2023-11-07 16:39:41 -0300

    Format bot response

--- common/src/commonMain/kotlin/chat/sphinx/common/chatMesssageUI/BotResponse.kt
+++ common/src/commonMain/kotlin/chat/sphinx/common/chatMesssageUI/BotResponse.kt
@@ -1,31 +1,56 @@
 package chat.sphinx.common.chatMesssageUI
 
-import androidx.compose.foundation.layout.Box
+import Roboto
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import chat.sphinx.common.components.MessageMenu
-import chat.sphinx.common.components.chat.KebabMenu
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import chat.sphinx.common.models.ChatMessage
 import chat.sphinx.common.viewmodel.chat.ChatViewModel
-import com.multiplatform.webview.web.WebView
-import com.multiplatform.webview.web.rememberWebViewState
-import com.multiplatform.webview.web.rememberWebViewStateWithHTMLData
+import chat.sphinx.wrapper.message.isPaidTextMessage
 
 @Composable
 fun BotResponse(
     chatMessage: ChatMessage,
     chatViewModel: ChatViewModel
-){
+) {
     chatMessage.botResponse?.let { contentHtml ->
-        val state = rememberWebViewStateWithHTMLData(
-            contentHtml,
-            null,
-            "utf-8",
-            "text/html",
-            null
-        )
-    WebView(state)
 
+        // Extracting the text from the HTML and reformatting it
+        val formattedText = contentHtml
+            .replace(Regex("<div style=\"[^\"]*\"><div style=\"[^\"]*\">"), "\n") // Matches the start of a label
+            .replace(Regex("</div><div style=\"[^\"]*\">"), "\n") // Matches the start of a command
+            .replace("</div>", "") // Matches the end of a command
+            .replace(Regex("<.*?>"), "") // Removes all other HTML tags
+            .trim()
+
+        val topPadding = if (chatMessage.message.isPaidTextMessage && chatMessage.isSent) 44.dp else 12.dp
+
+        Row(
+            modifier = Modifier
+                .padding(12.dp, topPadding, 12.dp, 12.dp)
+                .wrapContentWidth(if (chatMessage.isSent) Alignment.End else Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Text(
+                formattedText,
+                style = TextStyle(
+                    fontWeight = FontWeight.W400,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontSize = 13.sp,
+                    fontFamily = Roboto,
+                )
+            )
+        }
     }
 }
