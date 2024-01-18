e8d800c65ee54e9727e91cad3d9f36d5432f5937
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-01-18 09:58:56 -0300

    Webview library moved from SNAPSHOT version to final version

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -49,7 +49,7 @@ kotlin {
                 implementation("org.cryptonode.jncryptor:jncryptor:1.2.0")
                 implementation("com.soywiz.korlibs.korio:korio:$korioVersion")
                 implementation("com.soywiz.korlibs.korau:korau:$korauVersion")
-                api("io.github.kevinnzou:compose-webview-multiplatform:1.8.0-SNAPSHOT")
+                api("io.github.kevinnzou:compose-webview-multiplatform:1.8.0")
             }
         }
         val commonTest by getting {
