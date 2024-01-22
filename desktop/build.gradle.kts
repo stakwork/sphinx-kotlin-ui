9639da94d29752ed16596f6aeb721d5720ab6ef7
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-01-22 11:43:03 -0300

    Version 1.0.22

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -98,7 +98,7 @@ compose.desktop {
 
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "Sphinx"
-            packageVersion = "1.0.21"
+            packageVersion = "1.0.22"
 
             val iconsRoot = project.file("../common/src/desktopMain/resources/images")
             val sphinxProperties = Properties().apply {
